cbccda219dd4e5cc99bd75dfb8edd29dcfe2026d
pavel <pavel.sedlacek2004@gmail.com>
2022-04-20 15:48:58 +0200

    alfa

--- alfa.sql
+++ alfa.sql
@@ -7,6 +7,20 @@ FROM Osetruje
 WHERE Osetruje.id IS NULL;
 
 # Alfa2: Vypište všechny mloky, které nemá rád nikdo, kdo má rád pávy.
-
+SELECT *
+FROM Zvirata
+         JOIN Druhy ON Druhy.id = Zvirata.druh AND Druhy.nazev LIKE 'mlok'
+         LEFT JOIN Ma_rad ON Ma_rad.druh = Zvirata.druh
+         LEFT JOIN Osetrovatele ON Ma_rad.osetrovatel = Osetrovatele.id
+         LEFT JOIN Ma_rad M1 ON M1.osetrovatel = Osetrovatele.id
+         LEFT JOIN Druhy D1 ON D1.id = M1.id AND D1.nazev LIKE 'pav'
+WHERE D1.id IS NULL;
 
 # Alfa3: Vypište všechny ošetřovatele, kteří mají rádi stonožky a současně orly.
+SELECT Osetrovatele.id
+FROM Osetrovatele
+         JOIN Ma_rad ON Ma_rad.osetrovatel = Osetrovatele.id
+         LEFT JOIN Druhy D1 ON D1.id = Ma_rad.druh AND D1.nazev LIKE 'orel'
+         LEFT JOIN Druhy D2 ON D2.id = Ma_rad.druh ANd D2.nazev LIKE 'stonozka'
+WHERE D1.id IS NOT NULL
+  AND D2.id IS NOT NULL;
